94434b8996cee3d2d5de784577abd7e7c742cd58
郑旭钡 <gfazba@gmail.com>
2022-09-22 09:47:47 +0800

    feat: add storage

---
+++ src/plugins/storage.ts
@@ -0,0 +1,37 @@
+class MyCache {
+  private storage: Storage
+  constructor(isLocal = true) {
+    this.storage = isLocal ? localStorage : sessionStorage
+  }
+
+  setItem(key: string, value: any) {
+    if (typeof value === 'object') value = JSON.stringify(value)
+    this.storage.setItem(key, value)
+  }
+
+  getItem(key: string) {
+    const val = this.storage.getItem(key)
+    return val ? JSON.parse(val) : val
+  }
+
+  removeItem(key: string) {
+    this.storage.removeItem(key)
+  }
+
+  clear() {
+    this.storage.clear()
+  }
+
+  key(index: number) {
+    return this.storage.key(index)
+  }
+
+  length() {
+    return this.storage.length
+  }
+}
+
+const localCache = new MyCache()
+const sessionCache = new MyCache(false)
+
+export { localCache, sessionCache }
